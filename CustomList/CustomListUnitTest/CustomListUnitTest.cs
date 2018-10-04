3367c70078936a6c80a6fa64b60e6fbd4674e452
MR-Design <two7city@gmail.comMR-Design>
2018-10-04 18:56:55 -0500

    Finished Tests For Add Methode

--- CustomList/CustomListUnitTest/CustomListUnitTest.cs
+++ CustomList/CustomListUnitTest/CustomListUnitTest.cs
@@ -11,6 +11,89 @@ namespace CustomListUnitTest
         [TestMethod]
         public void TestMethod1()
         {
+            //Arrange
+            CList<int> customList = new CList<int>();
+            int value = 16;
+
+            //Act
+            customList.Add(value);
+
+            //Assert
+            Assert.AreEqual(value, customList[0]);
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            CList<int> customList = new CList<int>();
+            int value = 16;
+            int countValue = 1;
+
+            //Act
+            customList.Add(value);
+
+            //Assert
+            Assert.AreEqual(customList.Count, countValue);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            CList<int> customList = new CList<int>();
+            int value = 16;
+            int value1 = 20;
+
+
+            //Act
+            customList.Add(value);
+            customList.Add(value1);
+
+            //Assert
+            Assert.AreEqual(value, customList[0]);
+        }
+
+        /////////////////////////////////////////////////////////////
+        [TestMethod]
+        public void TestByIndexOF_list_Remove()
+        {
+            //Arrange
+            CList<int> customList = new CList<int>();
+            int value = 10;
+            int value1 = 20;
+
+            //Act
+            customList.Add(value);
+            customList.Add(value1);
+            customList.Remove(value1);
+
+
+            //Assert
+            Assert.AreEqual(value, customList[0]);
+        }
+
+        public void TestByCount_list_Remove()
+        {
+            //Arrange
+            CList<int> customList = new CList<int>();
+            int value = 10;
+            int value2 = 20;
+            int CountValue = 1;
+
+            //Act
+            customList.Add(value);
+            customList.Add(value2);
+            customList.Remove(20);
+
+
+            //Assert
+            Assert.AreEqual(CountValue, customList.Count);
+        }
+
+    
+       
     }
 }
+
+
