11c11f9884853bc5e3b04353dee0136a5f67b6da
MR-Design <two7city@gmail.comMR-Design>
2018-10-04 18:43:01 -0500

    Setting UP My Filles

---
+++ CustomList/CustomListUnitTest/CustomListUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+
+namespace CustomListUnitTest
+{
+    [TestClass]
+    public class CustomListUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
